aa438351cc47b3e98e4f7f9c1b9196931f37cbb0
Alex Macklen <sunray1@clara.co.uk>
2016-01-12 09:54:02 +0000

    Updated to latest versions of libraries

--- eShop/App_Start/BundleConfig.cs
+++ eShop/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace eShop
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/sticky-footer-navbar.css"));
         }
     }
 }
